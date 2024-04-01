f27f7ebe9c8f9c251d99b4195bb3b56898e1033b
SienneR <rodwells@hawaii.edu>
2024-03-31 17:33:40 -1000

    Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from '../app/about/about.component'
 
 const routes: Routes = [
   { path: "", redirectTo: "/vulnerabilities", pathMatch: "full" },
+  { path: "about", component: AboutComponent },
 ];
 
 @NgModule({
